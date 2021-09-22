3deaa947d252d7bc2c73beb20bb123352a8b58f9
utatanetaro <87312404+utatanetaro@users.noreply.github.com>
2021-09-22 13:09:15 +0900

    Create ClipButton

--- Screens/ArticleScreen.js
+++ Screens/ArticleScreen.js
@@ -1,23 +1,38 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { WebView } from 'react-native-webview';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addClip, deleteClip } from '../store/actions/user'
 import 'react-native-gesture-handler';
+import ClipButton from "../components/ClipButton";
 
 const ArticleScreen = (props) =>{
   const { route } = props;
   const { article } = route.params;
   const dispatch = useDispatch();
 
+  const user = useSelector(state => state.user);
+  const { clips } = user;
+
+  const isClipped = () => {
+    return clips.some(clip => clip.url === article.url);
+  }
+
+  const toggleClipe = () => {
+    if(isClipped()){
+      dispatch(deleteClip({clip: article}))
+    }else{
+      dispatch(addClip({clip: article}))
+    }
+  }
+
+
   return(
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity　onPress={()=>{dispatch(addClip({clip: article}))}}>
-        <Text style={{margin: 10, fontSize:30}}>ADD CLIP</Text>
-      </TouchableOpacity>
-      <TouchableOpacity　onPress={()=>{dispatch(deleteClip({clip: article}))}}>
-        <Text style={{margin: 10, fontSize:30}}>DELETE CLIP</Text>
-      </TouchableOpacity>
+      <ClipButton
+      onPress={toggleClipe}
+      enabled={isClipped()}
+      />
       <WebView source={{uri:article.url}} />
     </SafeAreaView>
   );
